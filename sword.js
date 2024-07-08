85b85f8e117449742b0f6a73ed8878f154f3e61d
sirsleon <seanjlua10@gmail.com>
2024-07-08 23:59:49 +1000

    SWORD V0.2

--- sword.js
+++ sword.js
@@ -10,12 +10,10 @@ var column = 0; // y axis
 var gameOver = false;
 var word = "";
 import { eligibleWords } from "./swordBank.js";
-// var eligibleWords = ["about", "above", "apple", "angle", "beach", "begin", "black", "brave", "bread", "brick", "bring", "broad", "brown", "brush", "cabin", "camel", "candy", "chain", "chair", "charm", "chase", "check", "chess", "chill", "china", "clean", "clear", "climb", "clock", "cloud", "coach", "coast", "color", "craft", "crazy", "cream", "creek", "crime", "cross", "crown", "cycle", "dance", "death", "delay", "demon", "devil", "dream", "drink", "drive", "earth", "enemy", "equal", "error", "event", "faith", "false", "favor", "fever", "field", "fight", "final", "first", "flame", "flash", "float", "floor", "flour", "focus", "force", "frame", "fresh", "front", "fruit", "glass", "globe", "grape", "grass", "great", "green", "guard", "guess", "habit", "happy", "heart", "heavy", "honey", "horse", "house", "human", "hurry", "image", "index", "input", "judge", "juice", "kneel", "knife", "laugh", "learn", "light", "limit", "liver", "lodge", "loose", "lucky", "lunch", "magic", "march", "match", "maybe", "means", "meant", "media", "metal", "might", "model", "money", "month", "motor", "mount", "mouse", "music", "naked", "nasty", "never", "night", "noble", "noise", "north", "nurse", "occur", "ocean", "offer", "often", "order", "other", "paint", "panel", "paper", "party", "peace", "piano", "piece", "pilot", "place", "plane", "plant", "plate", "point", "power", "press", "price", "pride", "prime", "prize", "prove", "queen", "quiet", "quick", "radio", "raise", "reach", "ready", "refer", "reply", "right", "river", "robot", "rough", "round", "route", "royal", "ruler", "salad", "scale", "scene", "score", "sense", "serve", "sharp", "sheep", "shelf", "shell", "shift", "shirt", "shock", "shore", "short", "sight", "skill", "slave", "sleep", "slide", "smile", "smoke", "snake", "solid", "solve", "sound", "south", "space", "spare", "speak", "spell", "spend", "spice", "spike", "spill", "split", "spoon", "sport", "spray", "stack", "staff", "stage", "stand", "start", "state", "steam", "steel", "stick", "still", "stone", "store", "storm", "story", "study", "stuff", "sugar", "suite", "sunny", "super", "sweet", "swept", "swing", "table", "taste", "teach", "thank", "their", "there", "these", "thing", "think", "third", "those", "throw", "thumb", "tiger", "tired", "title", "today", "topic", "total", "touch", "tough", "tower", "track", "trade", "train", "treat", "trend", "trial", "trick", "trust", "truth", "twice", "uncle", "under", "union", "unity", "upper", "upset", "urban", "usual", "valid", "value", "vapor", "video", "virus", "visit", "vital", "vivid", "voice", "watch", "water", "wheat", "wheel", "where", "which", "while", "white", "whole", "woman", "worry", "worse", "worth", "would", "wound", "write", "wrong", "yield", "young", "youth"];
 
 // On page load, initialise the game
 window.onload = function() {
     word = eligibleWords[Math.floor(Math.random() * eligibleWords.length)];
-    
     initialise();
 }
 
@@ -95,7 +93,7 @@ function validateInput() {
             return true
         }
         else {
-            alert("Please enter a valid word, your guess " + guess + " is not valid.")
+            alert("Please enter a valid word, your guess " + guess.toUpperCase() + " is not valid.")
             return false
         }
 }
@@ -134,7 +132,6 @@ function update() {
 
         if (correct == width) {
             gameOver = true;
-            alert("You win!")
         }
     }
 }
